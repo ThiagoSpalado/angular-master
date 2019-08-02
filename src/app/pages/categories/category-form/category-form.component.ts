2c2a3fce4ca5fd21b2e2486d54b7df40fea6673f
ThiagoSpalado <thiagospalado@gmail.com>
2019-08-01 23:38:38 -0300

    caretory-form-html_error ngif

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -120,9 +120,9 @@ private actionsForError(error){
   
   this.submittingForm = false;
 
-  if(error.status === 422)
-     this.serverErrorMessages = JSON.parse(error._body).errors;
-  else
-       this.serverErrorMessages = ["Falha na comunicação com o servidor, por favor tente mais tarde"];
+  // if(error.status === 422)
+  //    this.serverErrorMessages = JSON.parse(error._body).errors;
+  // else
+  //      this.serverErrorMessages = ["Falha na comunicação com o servidor, por favor tente mais tarde"];
 }
 }
