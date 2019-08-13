113c029c7d27b71a6830e29ff910120e450a312c
ThiagoSpalado <thiagospalado@gmail.com>
2019-08-13 00:52:16 -0300

    page-entries

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,5 +1,7 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
 import {Category} from "./pages/categories/shared/category.model";
+import { Entry } from "./pages/entries/shared/entry.model";
+
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
@@ -11,6 +13,10 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 5, name: 'Freelas', description:'Trabalhos com freelancer'}
         ];
 
-        return {categories}
+        const entries: Entry[] = [
+            { id: 1, name: 'Moradia', categoryId: categories[0].id, category: categories[0], paid: true, date: "10/10/2018", amount: "70,00", type: "expense", description: "descricao da despesa" } as Entry
+        ];
+
+        return {categories, entries}
     }
 }
